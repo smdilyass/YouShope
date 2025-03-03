5273e54b1e2abcf128b04950ec9da18484c31187
smdilyass <smdilyass@gmail.com>
2025-03-03 11:05:05 +0000

    i creat the Product factory

--- laravel/database/factories/ProductFactory.php
+++ laravel/database/factories/ProductFactory.php
@@ -17,7 +17,9 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->unique()->randomElement(['Laracel cap','Tshirt','Blanket','sweater']),
+            'description' => $this->faker->paragraph(2),
+            'price' => $this->faker->numberBetween(5_00 ,45_00),
         ];
     }
 }
