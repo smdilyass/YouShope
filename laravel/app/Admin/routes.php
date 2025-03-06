05010c7c2b3de427fe0c56c7b9bb99ebe7591a2e
smdilyass <smdilyass@gmail.com>
2025-03-06 13:27:03 +0000

    I Add admin controller, bootstrap, and routes for admin panel setup

---
+++ laravel/app/Admin/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Routing\Router;
+use OpenAdmin\Admin\Facades\Admin;
+use App\Http\Controllers\ProductController;
+use OpenAdmin\Admin\Controllers\UserController;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index')->name('home');
+    $router->resource('products', ProductController::class);
+    $router->resource('users', UserController::class);
+});
